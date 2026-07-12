a3d28a0269dc54a3d222dd615f90f4619adf3263
Mugdha-Tadwalkar <mugdhatadwalkar@gmail.com>
2026-07-12 04:44:00 +0000

    Implemented Embedding Model

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from langchain_community.document_loaders import TextLoader
 # This is LangChain's most commonly used text splitter.
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+#Required libraries for environment var
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+#loading env var from dotenv
+load_dotenv()
+print(os.getenv("GOOGLE_API_KEY"))
 #Step 1 : Document loading 
 # Create a loader object and tell it which file to read.
 loader = TextLoader("meeting.txt")
@@ -23,16 +30,30 @@ splitter = RecursiveCharacterTextSplitter(
 chunks = splitter.split_documents(documents)
 
 # Print the number of chunks created.
-print(f"Total Chunks: {len(chunks)}")
+#print(f"Total Chunks: {len(chunks)}")
 
 #enumerate gives you both index and a value. Basically it lets you loop through a list while keeping the track of index.
 # Print every chunk.
-for i, chunk in enumerate(chunks):
-    print("=" * 40)
-    print(f"Chunk {i+1}")
-    print("=" * 40)
-    print(chunk.page_content)
+# for i, chunk in enumerate(chunks):
+#     print("=" * 40)
+#     print(f"Chunk {i+1}")
+#     print("=" * 40)
+#     print(chunk.page_content)
     
 #page_content is an attribute (property) of a LangChain Document object that stores the actual text of the document or chunk.
-
 #chunk_size is the maximum size of a chunk, not the exact size. RecursiveCharacterTextSplitter tries to keep meaningful text together and only splits when necessary, while chunk_overlap repeats a small part of the previous chunk to preserve context.
+
+# Step 5 : Creating the embedding model
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001"
+)
+
+# Step 6 : Converting the chunks into embedding
+
+vector = embeddings.embed_query(chunks[0].page_content)
+print("1st chunk")
+print(chunks[0].page_content)
+print(len(vector))
+print(vector[:10])
+    
